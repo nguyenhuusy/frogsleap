3b598c1fa9f0caf8b5438df2792e20a1f3c7aaf8
nguyenhuusy <52096108+nguyenhuusy@users.noreply.github.com>
2020-04-08 16:34:35 +0700

    Updates

---
+++ precache-manifest.7d5bf1ca0f8aaaa8f9dd2b270add9ef5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91bc776217b3b606a5385470e4d9ffb3",
+    "url": "/frogsleap/index.html"
+  },
+  {
+    "revision": "f1b12b2bec5d3f730a81",
+    "url": "/frogsleap/static/css/main.ffe81ccb.chunk.css"
+  },
+  {
+    "revision": "bc54ad4d0a38b947a985",
+    "url": "/frogsleap/static/js/2.45ece556.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/frogsleap/static/js/2.45ece556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1b12b2bec5d3f730a81",
+    "url": "/frogsleap/static/js/main.705c5614.chunk.js"
+  },
+  {
+    "revision": "b4ced681baf6694014d4",
+    "url": "/frogsleap/static/js/runtime-main.6b6a5b6a.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/frogsleap/precache-manifest.7d5bf1ca0f8aaaa8f9dd2b270add9ef5.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/frogsleap/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
