36438a9768a860c9c6de8ad041612671c929fa3a
nguyenhuusy <52096108+nguyenhuusy@users.noreply.github.com>
2020-04-09 16:46:48 +0700

    second commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,11 +7,12 @@ class Header extends Component {
   render() {
     return (
       <div className="header">
-        <video id="video_background" preload="auto" autoplay="true" loop="loop" muted volume="0">
-                        <source src="https://bitly.com.vn/s9Wnq" type="video/webm"/>
-                        <source src="https://bitly.com.vn/s9Wnq" type="video/ogg ogv" codecs="theora, vorbis"/>
-                        <source src="https://bitly.com.vn/s9Wnq" type="video/mp4"/>
-                </video>
+         {/* <video id="video_background" preload="auto" autoplay="true" loop="loop" muted volume="0">
+                        <source src="https://player.vimeo.com/video/245391977" type="video/webm"/>
+                        <source src="https://player.vimeo.com/video/245391977" type="video/ogg ogv" codecs="theora, vorbis"/>
+                        <source src="https://player.vimeo.com/video/245391977" type="video/mp4"/>
+                </video>  */}
+                 <iframe id="video_background" src="https://player.vimeo.com/video/245391977?autoplay=1&loop=1&volume=0" preload="auto" muted volume="0" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe> 
         <nav className="nav">
           <div className="nav-logo-wrap">
             <img src="https://i.imgur.com/rTLOgiz.png" className="logo"></img>
